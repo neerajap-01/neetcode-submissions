87b13b85d14afb0bbd7371fabafcaf69a642c32a
Neeraj Pal <neerajap.a01@gmail.com>
2026-06-21 23:48:17 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let set = new Set();
+        let l = 0;
+        let maxSize = 0;
+
+        for(let r = 0; r < s.length; r++) {
+            while(set.has(s[r])) {
+                set.delete(s[l]);
+                l++
+            }
+
+            set.add(s[r]);
+            maxSize = Math.max(maxSize, set.size)
+        }
+
+        return maxSize;
+    }
+}

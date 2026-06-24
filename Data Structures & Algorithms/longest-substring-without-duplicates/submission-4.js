a6b64fa4fc67823a9c2ea4fdc34e2bd772f83cc5
Neeraj Pal <neerajap.a01@gmail.com>
2026-06-24 23:17:58 +0530

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+      const charSet = new Set();
+      let l = 0;
+      let ans = 0;
+
+      for(let r = 0; r < s.length; r++) {
+        while(charSet.has(s[r])) {
+          charSet.delete(s[l]);
+          l++;
+        }
+
+        charSet.add(s[r]);
+        ans = Math.max(ans, charSet.size);
+      }
+
+      return ans;
+    }
+}

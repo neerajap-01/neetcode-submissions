2900f1d1793acc5ba5d6fe539760cfda4e58f971
Neeraj Pal <neerajap.a01@gmail.com>
2026-07-19 21:51:22 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let minPrice = prices[0];
+
+        for(let i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}

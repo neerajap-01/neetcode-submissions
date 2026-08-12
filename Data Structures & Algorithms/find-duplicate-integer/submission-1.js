6c07133e32aa9b764fb5b03ae7a17064cc52c59a
Neeraj Pal <neerajap.a01@gmail.com>
2026-08-12 20:30:52 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let s = 0;
+        let f = 0;
+
+        while(true) {
+            s = nums[s];
+            f = nums[nums[f]];
+
+            if(s === f) break;
+        }
+
+        let s1 = 0;
+
+        while(true) {
+            s = nums[s];
+            s1 = nums[s1];
+
+            if(s1 === s) return s
+        }
+    }
+}

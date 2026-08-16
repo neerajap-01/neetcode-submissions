c2167ae0be4a068cbdf5c173a16b344577908123
Neeraj Pal <neerajap.a01@gmail.com>
2026-08-16 22:57:04 +0530

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        const postOrderTraversal = (node) => {
+            if(!node) return 0;
+
+            const left = postOrderTraversal(node.left);
+            if(left == -1) return -1
+            const right = postOrderTraversal(node.right);
+            if(right == -1) return -1
+
+            const isValid = Math.abs(left - right) <= 1;
+
+            return isValid ? Math.max(left, right) + 1 : -1
+        }
+
+        return postOrderTraversal(root) !== -1
+    }
+}

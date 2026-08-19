201aae565ace52c6db5b961cf5522f7bd983166f
Neeraj Pal <neerajap.a01@gmail.com>
2026-08-19 16:11:53 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {character[]} tasks
+     * @param {number} n
+     * @return {number}
+     */
+    leastInterval(tasks, n) {
+        const freq = new Map();
+
+        for(let task of tasks) {
+            freq.set(task, (freq.get(task) || 0) + 1)
+        }
+
+        const maxHeap = new MaxPriorityQueue();
+        for(let count of freq.values()){
+            maxHeap.enqueue(count)
+        }
+
+
+        const queue = [];
+        let time = 0;
+
+        while(maxHeap.size() > 0 || queue.length > 0) {
+            time++
+
+            if(maxHeap.size() > 0) {
+                let count = maxHeap.dequeue();
+                count--;
+
+                if(count > 0) {
+                    queue.push({ task: count,  unlockTime: time + n })
+                }
+            }
+
+            if(queue.length > 0 && queue[0].unlockTime == time) {
+                const readyTask = queue.shift();
+                maxHeap.enqueue(readyTask.task)
+            }
+        }
+
+        return time;
+    }
+}

b227b05fdffd3a63d8e604e766a351363a97512e
Neeraj Pal <neerajap.a01@gmail.com>
2026-08-31 21:10:53 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(nums) {
+        nums = nums.sort((a,b) => a - b)
+        const res = [];
+        const cur = [];
+
+        const dfs = (i) => {
+            if(i >= nums.length) {
+                res.push([...cur]);
+                return;
+            };
+
+            const el = nums[i];
+            cur.push(el);
+            dfs(i+1);
+
+            while (el === nums[i]) i++
+            cur.pop();
+            dfs(i)
+        }
+
+        dfs(0);
+
+        return res;
+    }
+}

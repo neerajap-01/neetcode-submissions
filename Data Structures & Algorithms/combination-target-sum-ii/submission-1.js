f9635622adcab746b40e9e8db2165cf01288adb8
Neeraj Pal <neerajap.a01@gmail.com>
2026-09-01 21:26:03 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        candidates = candidates.sort((a,b) => a - b);
+        const res = [];
+        const cur = [];
+
+        let sum = 0;
+        const dfs = (i) => {
+            if(sum === target) {
+                res.push([...cur]);
+                return;
+            }
+
+            if(sum > target || i >= candidates.length) return;
+
+            const el = candidates[i];
+            cur.push(el);
+            sum += el;
+            dfs(i + 1);
+
+            i++
+            while(i <= candidates.length && el == candidates[i]) i++
+            cur.pop();
+            sum -= el;
+            dfs(i)
+        }
+
+        dfs(0);
+        return res;
+    }
+}

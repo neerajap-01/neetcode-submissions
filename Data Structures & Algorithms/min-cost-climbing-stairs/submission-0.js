ec2a8017d5e9a407a97c06a60a0548b736d94240
Neeraj Pal <neerajap.a01@gmail.com>
2026-09-14 12:08:00 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        cost.push(0);
+        for(let i = cost.length - 3; i >= 0; i--) {
+            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        }
+
+        return Math.min(cost[0], cost[1]);
+    }
+}

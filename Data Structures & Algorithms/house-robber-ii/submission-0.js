4aaef7830f024e465701b72aec8d8a36bbd9c9b7
Neeraj Pal <neerajap.a01@gmail.com>
2026-09-14 14:20:38 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        const helper = (arr) => {
+            let twoHousesAgo = 0;
+            let oneHouseAgo = 0;
+
+            for(let i = 0; i < arr.length; i++) {
+                const temp = Math.max(arr[i] + twoHousesAgo, oneHouseAgo)
+                twoHousesAgo = oneHouseAgo;
+                oneHouseAgo = temp
+            }
+            return oneHouseAgo;
+        }
+
+        return Math.max(nums[0], helper(nums.slice(0,nums.length - 1)), helper(nums.slice(1)))
+    }
+}

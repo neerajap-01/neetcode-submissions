1e9b4ff26e4fea9bd118c7b8877caea5e9a190e7
Neeraj Pal <neerajap.a01@gmail.com>
2026-09-20 18:34:33 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a,b) => a[0] - b[0]);
+        const res = [intervals[0]];
+
+        for(let i = 1; i < intervals.length; i++) {
+            if(intervals[i][0] <= res[res.length - 1][1]) {
+                res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1])
+            } else {
+                res.push(intervals[i])
+            }
+        }
+
+        return res;
+    }
+}
